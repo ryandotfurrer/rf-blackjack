ffa1e04f2399edf58a09e54b319239570d0a5af8
Ryan Furrer <rfurrertech@gmail.com>
2022-07-10 18:35:47 -0400

    Began writing JavaScript for game fucntions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-**/.DS_Store
+let playerCards = document.querySelector("#player-cards");
+let playerScore = document.querySelector("#player-score");
+let newCardBtn = document.querySelector("#new-card-btn");
+let newGameBtn = document.querySelector("#new-game-btn");
+let cardOne = randomCard();
+let cardTwo = randomCard();
+let cards = [cardOne, cardTwo];
+
+function randomCard() {
+	return Math.floor(Math.random() * 11) + 1;
+}
+
+newGameBtn.addEventListener("click", () => {
+	playerCards.textContent = `Cards: ${cards}`;
+});
+
+newCardBtn.addEventListener("click", () => {
+	cards.push(randomCard());
+	playerCards.textContent = `Cards: ${cards}`;
+});
+
+// function getTotalScore() {
+// 	for (i = 0; i <= cards.length; i++) {
+// 		let score = cards[i];
+// 		return score;
+// 	}
+// }
